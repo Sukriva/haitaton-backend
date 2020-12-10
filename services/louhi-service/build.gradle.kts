abcc5f93e782222ad946bb13c5aa80759a2c0da1
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2020-12-10 15:18:40 +0200

    HAI-361: trying to get hawt.io to work

--- services/louhi-service/build.gradle.kts
+++ services/louhi-service/build.gradle.kts
@@ -4,6 +4,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "fi.hel.haitaton"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+val camelVersion = "3.6.0"
+val hawtIoVersion = "2.12.0"
 val postgreSQLVersion = "42.2.18"
 val springDocVersion = "1.4.8"
 val geoJsonJacksonVersion = "1.14"
@@ -43,29 +45,35 @@ springBoot {
 plugins {
 	id("org.springframework.boot") version "2.3.4.RELEASE"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.20"
+	kotlin("jvm") version "1.4.21"
 	// Gives kotlin-allopen, which auto-opens classes with certain annotations
-	kotlin("plugin.spring") version "1.4.20"
+	kotlin("plugin.spring") version "1.4.21"
 	// Gives kotlin-noarg for @Entity, @Embeddable
-	kotlin("plugin.jpa") version "1.4.20"
+	kotlin("plugin.jpa") version "1.4.21"
 	idea
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web") {
+		exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+	}
+	implementation("org.springframework.boot:spring-boot-starter-undertow") {
+		exclude(group = "io.undertow", module = "undertow-websockets-jsr")
+	}
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.apache.camel.springboot:camel-spring-boot-starter:$camelVersion")
+	implementation("org.apache.camel.springboot:camel-stream-starter:$camelVersion")
+	implementation("org.apache.camel.springboot:camel-quartz-starter:$camelVersion")
+	implementation("org.apache.camel:camel-management:$camelVersion")
+	implementation("io.hawt:hawtio-springboot:$hawtIoVersion")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.github.microutils:kotlin-logging:1.12.0")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("de.grundid.opendatalab:geojson-jackson:$geoJsonJacksonVersion")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.liquibase:liquibase-core")								// TODO: do we need org.springframework:spring-dao ?
-
+	implementation("org.liquibase:liquibase-core")
 	runtimeOnly("org.postgresql:postgresql:$postgreSQLVersion")
-	// H2 is used as embedded db for some simple low level Entity and Repository class testing
-	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.springdoc:springdoc-openapi-ui:$springDocVersion")
-
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
@@ -74,13 +82,7 @@ dependencies {
 	testImplementation("com.willowtreeapps.assertk:assertk-jvm:$assertkVersion")
 	testImplementation("org.testcontainers:junit-jupiter:1.15.0")
 	testImplementation("org.testcontainers:postgresql:1.15.0")
-	// Spring Boot Management
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
-
-//tasks.withType<Test> {
-//	useJUnitPlatform()
-//}
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
